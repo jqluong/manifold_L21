d92e4b6bd929769c2134728f19f76ccadd8638db
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2021-05-21 00:16:54 -0700

    update l21 again

--- prox_l21.m
+++ prox_l21.m
@@ -1,5 +1,5 @@
 %% The L2,1 proximal operator
-function V_new = prox_l21(V,c,functions)
+function V_new = prox_l21(V,c)
     g = size(V,2);
     for i = 1:size(V,2)
         Z_hold = V(:,i);
@@ -8,7 +8,7 @@ function V_new = prox_l21(V,c,functions)
     end
 
     alpha = max(0,1 - c./g);
-    V_new = diag(alpha)*V;
+    V_new = V*diag(alpha)';
     
 
 end
