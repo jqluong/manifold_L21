c83211eeb02f28a9b9a963cd57a9591966585b4f
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2021-05-21 00:03:30 -0700

    trying to implement l21

--- madmm_l21.m
+++ madmm_l21.m
@@ -1,7 +1,7 @@
 function X = madmm_l21(x0,functions,params)
 
-% set reg term: g(x) = |v(X)|_2,1 (sum of norm over columns)
-functions.fun_g = @(Z)sum(sqrt(sum(functions.fun_v(Z).^2,1)));
+% set reg term: g(x) = |v(X)|_2,1 (sum of norm over rows)
+%functions.fun_g = @(Z)sum(sqrt(sum(functions.fun_v(reshape(Z,numel(Z)/2,2).^2,2))));
 
 T = params.max_iter; %number of iterations
 
@@ -19,14 +19,14 @@ Z = functions.fun_v(x0);
 U = zeros(size(Z));
 
 
-original_cost = @(x)functions.fun_f(x) + params.lambda*functions.fun_g(x)
+%original_cost = @(x)functions.fun_f(x) + params.lambda*functions_g(functions,x);
 keep_cost = zeros(T,1);
 
 if params.is_plot, fig = animatedline; title('cost'); end
 
 for step = 1:T   
     
-    keep_cost(step) = original_cost(X);    
+    keep_cost(step) = original_cost(functions,params,X);    
         
     if params.is_plot, addpoints(fig,step,keep_cost(step)); drawnow(); end
 
@@ -65,3 +65,16 @@ if params.is_plot, figure, subplot(121),colormap, imagesc(x0), subplot(122), ima
 
 end
 
+function oc = original_cost(functions,params,x)
+    oc = functions.fun_f(x) + params.lambda*functions_g(functions,x);
+end
+
+function g = functions_g(functions,Z)
+    g = 0;
+    for i = 1:size(Z,2)
+        Z_hold = Z(:,i);
+        Z_hold = functions.fun_v(Z_hold);
+        Z_hold = reshape(Z_hold,numel(Z_hold)/2,2);
+        g = g + sum(sqrt(sum(Z_hold.^2,2)));
+    end
+end
