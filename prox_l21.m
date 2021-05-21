2da05eda0ddc8862bcb145f3ae22d380d5d17c9a
jqluong <jackaham_luongcoln@mail.fresnostate.edu>
2021-05-21 00:14:47 -0700

    still trying on l21, messing with proximal operator

--- prox_l21.m
+++ prox_l21.m
@@ -1,7 +1,13 @@
 %% The L2,1 proximal operator
-function V_new = prox_l21(V,c)
+function V_new = prox_l21(V,c,functions)
+    g = size(V,2);
+    for i = 1:size(V,2)
+        Z_hold = V(:,i);
+        Z_hold = reshape(Z_hold,numel(Z_hold)/2,2);
+        g(i) = sum(sqrt(sum(Z_hold.^2,2)));
+    end
 
-    alpha = max(0,1 - c./sqrt(sum(V.^2,2)));
+    alpha = max(0,1 - c./g);
     V_new = diag(alpha)*V;
     
 
